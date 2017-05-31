4a3c7ff8aa3a34803b2d8a62f522bd1de3e72720
Filmon <filmon.mengisteab@yahoo.com>
2017-05-31 10:35:47 -0700

    Modify encrypt method to process spaces

--- ruby/secret_agents.rb
+++ ruby/secret_agents.rb
@@ -2,29 +2,34 @@
 #=============
 # Prompt user input and store value in a variable
 # For every character in the string (for index 0 to length of string)do the following
-		# If character is space
-				# do nothing
-		# Otherwise 
-				# replace character with character [index + 1]
+# If character is space
+# do nothing
+# Otherwise
+# replace character with character [index + 1]
 
 # Decrypt method Pseudo code
 #=======
 # Prompt user input and store value in a variable
 # For every character in the string (for index 0 to length of string)do the following
-		# If character is space
-				# do nothing
-		# Otherwise 
-				# replace character with character [index -1]
+# If character is space
+# do nothing
+# Otherwise
+# replace character with character [index -1]
 
 
 def encrypt(word)
-	i=0
-	new_word= ""
-	while i< word.length
-		new_word +=word[i].next
-			i +=1
-		end
-return new_word
+  i=0
+  new_word= ""
+  while i< word.length
+    if word[i]==" "
+      new_word +=word[i]
+      i +=1
+    else
+      new_word +=word[i].next
+      i +=1
+    end
+  end
+  return new_word
 end
 
 puts encrypt("ab cd")
