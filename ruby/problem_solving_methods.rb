15b6182dc466cc3ba4126dc329a789b8310ccf5b
Filmon <filmon.mengisteab@yahoo.com>
2017-06-09 17:35:03 -0700

    Add fibonacci method

--- ruby/problem_solving_methods.rb
+++ ruby/problem_solving_methods.rb
@@ -19,3 +19,20 @@ end
 arr = [42, 89, 23, 1, 1]
 p search_array(arr, 1)
 p search_array(arr, 24)
+
+# Fibonacci numbers method
+def fib(n)
+  arr = [0, 1]
+  i = 2
+  until arr.length == n
+    arr[ i] = arr [i -1] + arr[ i - 2]
+    i += 1
+  end
+  arr
+end
+
+p fib(6)
+p fib(100) - fib(99)  
+# We subtracted fib(99) from fib(100) to verify that the last 
+# number in the array would be 218922995834555169026 
+# instead of returning an array with many numbers
