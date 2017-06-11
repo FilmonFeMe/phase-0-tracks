b886295aa2ed259db56d925714e419b486337a30
Filmon <filmon.mengisteab@yahoo.com>
2017-06-11 05:22:51 -0700

    Add pseudo code to file

---
+++ ruby/alias_manager.rb
@@ -0,0 +1,12 @@
+#Pseudo code
+#==========
+#FOR a real name
+  #1.Swap first name and last name
+  #2. FOR all the letters of the first name
+    #convert a vowel to the next vowel
+    #convert a consonant to the next consonant
+    #leave any other special characters
+  #3. Repeat step 2 for last name
+  #4. Combine first name and last name
+#Repeat steps 1-4  for the next real name entered
+# Display real name and corresponding fake name for the entire data entered
