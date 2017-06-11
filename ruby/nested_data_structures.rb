37e5746da0d1d4c9cc8281f1fdd848840367a76b
Filmon <filmon.mengisteab@yahoo.com>
2017-06-10 17:01:28 -0700

    Create nested data structures using array and hash

---
+++ ruby/nested_data_structures.rb
@@ -0,0 +1,32 @@
+#Nested data structures
+countries = { 
+	"Eritrea" => { 
+					continent: "Africa",
+					population: 5500000,
+					cities_visited: ["Asmara", "Massawa", "Keren", "Mendefera", "Akordat", "Barentu", "Dekemhare"],
+					capital_city: "Asmara",
+					liked_weather: true,
+				},
+	"Germany" => {
+						continent: "Europe",
+						population: 80636124,
+						cities_visited: ["Dresden", "Berlin", "Frankfurt", "Leipzig", "Dortmund"],
+						capital_city: "Berlin",
+						liked_weather: false,
+					},
+
+	"USA" => {
+						continent: "North America",
+						population: 326474013,
+						cities_visited: ["San Jose", "San Diego", "San Francisco", "Los Angeles"],
+						capital_city: "Washington D.C.",
+						liked_weather: true,
+				},
+	"Netherlands" => {
+						continent: "Europe",
+						population: 17032845,
+						cities_visited: ["Amsterdam", "Rotterdam"],
+						capital_city: "Amsterdam",
+						liked_weather: true,
+					}
+}
