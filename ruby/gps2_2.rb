c5f7094e74cea7a888d0bb3bb332bf5ce27281f8
Filmon <filmon.mengisteab@yahoo.com>
2017-06-14 17:01:12 -0700

    Add update_quantity and display_items

--- ruby/gps2_2.rb
+++ ruby/gps2_2.rb
@@ -1,51 +1,59 @@
 # Initialize list
 # Method to create a list
 # input: string of items separated by spaces (example: "carrots apples cereal pizza")
-# steps: 
-  # break up a string into an array
-  # populate the hash
-  # set default quantity
-  # print the list to the console [use print method]
+# steps:
+#  	# break up a string into an array
+# 	# populate the hash
+# 	# set default quantity
+#  	# print the list to the console [use print method]
 # output: hash
 
 # Method to add an item to a list
 # input: list, item name, and optional quantity
 # steps:
-	# add item to list as key
-	# add corresponding quantity as a value
+#	# add item to list as key
+#	# add corresponding quantity as a value
 # output: hash
 
 # Method to remove an item from the list
 # input: list, item name
-# steps: 
-	# DELETE key-value pair from the list
+# steps:
+#	# DELETE key-value pair from the list
 # output: hash
 
 # Method to update the quantity of an item
 # input: list, item name, new quantity
 # steps:
-	# SET the value of the specific item name equal to the item quantity
+#	# SET the value of the specific item name equal to the item quantity
 # output: hash
 
 # Method to print a list and make it look pretty
 # input: list
 # steps:
-	# ITERATE  through the hash and display the item name and item quantity
+#	# ITERATE  through the hash and display the item name and item quantity
 # output: nil
 
 grocery_list = { } # initialize list
 
-def create_list( grocery_list, items)
-	item_array = items.split(" ")
-	item_array.each { |item| grocery_list[item] = 1}
-	# display_items(grocery_list)
-
-	grocery_list
+def display_items(grocery_list)
+  puts "\nThe Grocery List"
+  puts  "=============== "
+  grocery_list.each do |item_list, quantity|
+    puts "Please buy #{quantity} of #{item_list}."
+  end
 end
-# p grocery_list
-# items = "apple pizza cherry toiletries"
-# create_list(grocery_list, items)
-# p grocery_list
+
+def create_list( grocery_list, items)
+  item_array = items.split(" ")
+  item_array.each { |item| grocery_list[item] = 1}
+  display_items(grocery_list)
+
+  grocery_list
+end
+p grocery_list
+items = "apple pizza cherry toiletries"
+create_list(grocery_list, items)
+p grocery_list
 
 # def add_item(grocery_list, item_name, quantity = 1)
 # 	grocery_list[item_name] = quantity
@@ -58,9 +66,13 @@ end
 # p grocery_list
 
 def remove_item(grocery_list, item_name)
-	grocery_list.delete(item_name)
+  grocery_list.delete(item_name)
 end
 
-# remove_item(grocery_list, "apple")
+def update_quantity(grocery_list, item_name, quantity)
+  grocery_list[item_name] = quantity
+end
 
-# p grocery_list
+update_quantity(grocery_list, "apple", 150000)
+p grocery_list
+display_items(grocery_list)
