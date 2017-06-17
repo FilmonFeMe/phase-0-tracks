8e6802ce4836ef512d7114a493e86753c94c4e1c
Filmon <filmon.mengisteab@yahoo.com>
2017-06-16 17:23:59 -0700

    Include shout ruby module in two classess and add driver code

--- ruby/shout.rb
+++ ruby/shout.rb
@@ -1,14 +1,42 @@
+# module Shout
+#   def self.yell_angrily(words)
+#     words + "!!!" + " :("
+#   end
+
+#   def self.yelling_happily(word)
+#     word + "!!!"
+#   end
+# end
+
+# #Driver code
+# #==========
+# puts Shout::yell_angrily("ohhhh my bad")
+# puts Shout::yelling_happily("wow....awesome")
+
 module Shout
-  def self.yell_angrily(words)
+  def yell_angrily(words)
     words + "!!!" + " :("
   end
 
-  def self.yelling_happily(word)
+  def yelling_happily(word)
     word + "!!!"
   end
 end
 
+class Teacher
+  include Shout
+end
+
+class Student
+  include Shout
+end
+
 #Driver code
 #==========
-puts Shout::yell_angrily("ohhhh my bad")
-puts Shout::yelling_happily("wow....awesome")
+teacher = Teacher.new
+puts teacher.yell_angrily("get out")
+puts teacher.yelling_happily("yeah....all of my students passed")
+
+student = Student.new
+puts student.yell_angrily("Give me back my pencil")
+puts student.yelling_happily("yeah....I passed all of my exams")
