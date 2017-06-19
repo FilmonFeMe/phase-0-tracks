b40bfa9d1de221a16c978e2b93ca22b4c699f44e
Filmon <filmon.mengisteab@yahoo.com>
2017-06-18 23:40:09 -0700

    Add psedocode to word_guess file

---
+++ ruby/game/word_guess.rb
@@ -0,0 +1,31 @@
+#Word Guess Game
+#Psedocode 
+#===============
+
+#create a class for the game
+
+#	#define initializer method for the class
+#		#steps: initialize instance variables for the secret word, guessed letter, guessed word and number of attempts
+#		# output : instance object with instance variables set to initial values
+
+#	# define update_guessed_word method
+#		#input:
+#		#steps:
+#			# FOR each of secret word array letters
+#				# IF letter equals guessed letter 
+#					#SET guessed word value at index of letter in secret word to guessed letter
+#		#output: guessed word
+#	# define valid_letter? method
+#		#input: 
+#		#steps:
+#			# IF guessed letter is an English alphabet AND included in secret word
+#				-return true
+#			# otherwise
+#				-return false
+#		#output: boolean
+
+# 	# define ACCESSOR methods for instance variable attempt number and secret word
+# 	# define GETTER method for the guessed word instance variable
+# 	# define SETTER method for the guessed letter instance variable
+
+#	#define driver code
