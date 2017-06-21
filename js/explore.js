70c7e712ec4bf4c94d9c0aed9a4fd65ad87355d5
Filmon <filmon.mengisteab@yahoo.com>
2017-06-20 21:58:39 -0700

    Add reverse method to file

--- js/explore.js
+++ js/explore.js
@@ -1,14 +1,37 @@
 // Pseudo code method to reverse a string
 //-------------------------------------------------------------
 
-//define an empty variable to  store the  reversed word (reversedWord)
-
 // define function (reverse)
 //input : String
 //steps : 
+//	//define an empty variable to  store the  reversed word (reversedWord)
 //	// start iteration: for each character in the input word starting at the end 
 //	//add the character to the variable (reversedWord)
 // 	// end iteration
 //output :string
 
 // add driver code
+
+function reverse(originalWord) {
+   var wordSize = originalWord.length;
+    if (wordSize != 0 && originalWord != " ") {
+        var reversedWord = "";
+
+        for (var i = wordSize - 1; i > -1; i--) {
+            reversedWord += originalWord[i];
+        }
+        return reversedWord;
+    }else {
+    	console.log("You did not provide a string. Please type again.");
+    }
+}
+
+// check method
+// var originalWord = "hello";
+
+// console.log("The original word is '" + originalWord +"'" + " and it is '" + reverse(originalWord) +"' after reversing it.");
+
+//Driver code
+//==========
+
+
