934082537dac26bdfdfcfb995a399d05bebc28af
Filmon <filmon.mengisteab@yahoo.com>
2017-06-21 12:59:28 -0700

    Add reflection to file

--- ruby/gps6/my_solution.rb
+++ ruby/gps6/my_solution.rb
@@ -1,10 +1,10 @@
 # Virus Predictor
 
-# I worked on this challenge [by myself, with: ].
-# We spent [#] hours on this challenge.
+# I worked on this challenge by myself  with guide Laura Streb
+# I spent approximately 1.5 hours on this challenge.
 
 # EXPLANATION OF require_relative
-# require_relative : specifies file path relative current file
+# require_relative : specifies file path relative to current file where the require_relative is placed.
 #
 require_relative 'state_data'
 
@@ -127,3 +127,22 @@ VirusPredictor.virus_predictor(STATE_DATA)
 
 #=======================================================================
 # Reflection Section
+#-----------------------------------
+# What are the differences between the two different hash syntaxes shown in the state_data file?
+# => # the outer one uses strings as keys and the inner(nested hash) uses symbols as keys. Using strings as keys may be useful if we want to display the keys, for example "New York", if that is a symbol it would have been like this 'new_york', which is not good for displaying keys on the console. Using symbols as keys has two advantages, takes less memory storage and helps efficient usage as they are immutable(not changeable) where as strings are mutable (changeable) hence need many creations of the same string every time it is referenced with in the program. 
+
+# What does require_relative do? How is it different from require?
+#    # require_relative loads or imports contents of one file to another file.  
+#    # require is usually used to import from Ruby"s library path (us './' with file path), i.e. files properly installed and made accessible in the system.
+
+# What are some ways to iterate through a hash?
+#    # we can use the built in methods 'each' or 'map' to iterate through a hash referencing to the keys and values. If only keys are needed we can use 'keys' , and for values 'values' built in methods. For example,
+#            # hash.each do |key, value|
+#             #.......some code here........
+ #           # end
+
+# When refactoring virus_effects, what stood out to you about the variables, if anything?
+#    # Instance variables (declared with '@' before them) are accessible anywhere with in the class they are declared in. No need to pass them as arguments to methods, methods with in the class can access them directly regardless if the method is private, public or class method.
+
+# What concept did you most solidify in this challenge?
+#    # I have learned the DRY principle more clearly, the difference between require_relative and require,  and the instantiation of a class within itself using a class method. 
