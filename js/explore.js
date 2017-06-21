c8b3f5e8de0f45515e1087646626ddafd1a60d4c
Filmon <filmon.mengisteab@yahoo.com>
2017-06-20 21:30:02 -0700

    Add pseudo code to file

---
+++ js/explore.js
@@ -0,0 +1,14 @@
+// Pseudo code method to reverse a string
+//-------------------------------------------------------------
+
+//define an empty variable to  store the  reversed word (reversedWord)
+
+// define function (reverse)
+//input : String
+//steps : 
+//	// start iteration: for each character in the input word starting at the end 
+//	//add the character to the variable (reversedWord)
+// 	// end iteration
+//output :string
+
+// add driver code
